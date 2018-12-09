b5549270cf0d21313f7845b8801412302ffa2ffd
Haki <randy2995@gmail.com>
2018-12-10 06:19:57 +0700

    connect dbs demo

--- router.php
+++ router.php
@@ -1,4 +1,10 @@
 <?php
+include_once 'config.php';
+include_once "router.php";
+include_once 'controllers/user_controller.php';
+include_once 'controllers/novel_controller.php';
+include_once 'models/user_model.php';
+include_once 'models/novel_model.php';
 class Router {
 	//break url into uri and path
 	function parse(){
